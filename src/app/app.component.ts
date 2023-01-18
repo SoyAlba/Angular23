0aaa38906add1f4222e3550832a8a2f96819e5b2
SoyAlba <albamanas2003@gmail.com>
2023-01-18 08:54:20 +0100

    creando y haciendo que en el index se vea la info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `` ,
+  template: `<app-Alumno></app-Alumno>` ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
