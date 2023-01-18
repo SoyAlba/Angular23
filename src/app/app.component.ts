e67ff2693ae712e1bdc290d532f63c966f7a3735
SoyAlba <albamanas2003@gmail.com>
2023-01-18 09:45:53 +0100

    Usando estilos en los difernetes componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<app-Alumno></app-Alumno>` ,
+  template: `
+            <div>
+              <img src='{{imagen}}' width="150px">
+              <h1>{{encabezado}}</h1>
+              <app-Alumno></app-Alumno>
+            </div>` ,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Angular23';
+  encabezado='Datos Alumno';
+  nombre= 'Preico';
+  apellido='Delgado'
+  imagen ='https://salesianosrioja.com/wp-content/uploads/2016/03/Logo-Salesianos_vertical.png'
+
+  getNombreCompleto():string{
+    return this.nombre +' '+ this.apellido;
+  }
 }
