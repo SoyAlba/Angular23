3ce4d6d767bba48e64b5977d6fc86149331d1b96
SoyAlba <albamanas2003@gmail.com>
2023-02-01 10:08:14 +0100

    creando funcion para mostrar asolo hombres o mujeres con un select

--- src/app/Alumno/alumnoCount/alumnoCount.component.ts
+++ src/app/Alumno/alumnoCount/alumnoCount.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-alumnoCount',
   templateUrl: './alumnoCount.component.html',
   styleUrls: ['./alumnoCount.component.css']
 })
-export class AlumnoCountComponent implements OnInit {
+export class AlumnoCountComponent  {
+
+  @Input()
   todos:number=0;
+  @Input()
   hombres:number=0;
+  @Input()
   mujeres:number=0;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
