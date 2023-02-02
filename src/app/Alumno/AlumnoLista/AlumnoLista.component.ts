58e9f8fe67773a8ed89102d225e7d210fb438cec
SoyAlba <albamanas2003@gmail.com>
2023-02-02 11:51:53 +0100

    anadida funcion cabiarSelecion()

--- src/app/Alumno/AlumnoLista/AlumnoLista.component.ts
+++ src/app/Alumno/AlumnoLista/AlumnoLista.component.ts
@@ -6,12 +6,16 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./AlumnoLista.component.css']
 })
 export class AlumnoListaComponent implements OnInit {
+  selecion:string =;
 alumnos: any[]=[
   {nombre:'perico',apellidos:'delgado',direrccion:'segobia city',fnac:'12/2/2000',sexo:'hombre'},
   {nombre:'antonio',apellidos:'perez',direrccion:'murcia city',fnac:'2/7/2007',sexo:'hombre'},
   {nombre:'pedro',apellidos:'de lafuente',direrccion:'valencia city',fnac:'6/12/1973',sexo:'hombre'},
   {nombre:'maria',apellidos:'sanchez',direrccion:'zaragoza city',fnac:'7/2/2010',sexo:'mujer'}
 ]
+cabiarSelecion(opcionElegida:string):void{
+  this.selecion=opcionElegida;
+}
   constructor() { }
 
   ngOnInit() {
