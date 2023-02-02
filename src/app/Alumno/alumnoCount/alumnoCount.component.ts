c4ee3e0091207c620930dbb2cb5c85aa4b0ad4ed
SoyAlba <albamanas2003@gmail.com>
2023-02-02 11:52:29 +0100

    usando ng-container para hacer el bucle dentrp

--- src/app/Alumno/alumnoCount/alumnoCount.component.ts
+++ src/app/Alumno/alumnoCount/alumnoCount.component.ts
@@ -6,14 +6,18 @@ import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
   styleUrls: ['./alumnoCount.component.css']
 })
 export class AlumnoCountComponent  {
-
+  opcionElegida: string='Todos';
+  @Output()
+  globalElegido: EventEmitter<string>=new EventEmitter();
   @Input()
   todos:number=0;
   @Input()
   hombres:number=0;
   @Input()
   mujeres:number=0;
-
+  alCambiar(){
+    this.globalElegido.emit(this.opcionElegida);
+  }
   constructor() { }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,8 @@ import { AlumnoCountComponent } from './Alumno/alumnoCount/alumnoCount.component
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
